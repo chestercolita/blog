04e11fa7ae7f525d163bd66e2996f5f5d7be63cd
chestercolita <chester@nabepero.co.jp>
2020-12-10 11:07:30 +0800

    add sending email functionality using mailtrap for testing

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\PagesController;
 use App\Http\Controllers\PostsController;
 use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/sendmail', function () {
     return redirect()->route('dashboard');
 });
 
@@ -27,3 +28,9 @@ Route::get('/services', [PagesController::class, 'services'])->name('services');
 Route::resource('/posts', PostsController::class);
 
 Route::get('/admin', [AdminController::class, 'index']);
+
+Route::get('/sendmail', function () {
+    Mail::to('newuser@example.com')->send(new App\Mail\MailtrapExample);
+    return 'A message has been sent to Mailtrap!';
+
+});
